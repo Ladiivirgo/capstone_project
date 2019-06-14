8c5b4e952df5a66c7f28cb6b42b0f642a5668110
Anelia Garcia <aneliaalvarez@gmail.com>
2019-06-14 14:33:47 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  
+  root 'static_pages#home'
 
   get 'static_pages/home'
 
   get 'static_pages/help'
+  
+  get  'static_pages/about'
 
 resources :todo_lists do
   resources :todo_items do
